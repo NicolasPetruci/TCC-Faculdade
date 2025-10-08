99de0ba49a461698ad3deb99f66cb297fcb41e15
Nicolas Petruci Penga <dev@santacasadeassis.org.br>
2025-10-08 11:59:19 -0300

    movimentação direita

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,12 +1,13 @@
 extends CharacterBody2D
 @export var gravity := 800.0
 @export var jump_force := 350.0
+@export var speed := 300.0
+
 
 
 @onready var anim = $AnimatedSprite2D
 
 func _physics_process(delta):
-	#gravidade
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	else:
@@ -15,6 +16,15 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = -jump_force
 	
+	var direction = Input.get_axis("Esquerda", "Direita")
+	velocity.x = direction * speed
+	
+	if direction > 0:
+		anim.flip_h = false
+	elif direction < 0:
+		anim.flip_h = true
+		
+		
 	if not is_on_floor():
 		anim.play("Jumping")
 	else:
